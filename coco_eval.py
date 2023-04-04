dbb1ada3b99dd6426627f10e2f7f519703213df2
GILLYBOEUF <56752225+GILLYBOEUF@users.noreply.github.com>
2023-04-04 16:12:50 +0200

    Update coco_eval.py

--- coco_eval.py
+++ coco_eval.py
@@ -45,6 +45,7 @@ class CocoEvaluator:
 
     def synchronize_between_processes(self):
         for iou_type in self.iou_types:
+            print(self.eval_imgs[iou_type])
             self.eval_imgs[iou_type] = np.concatenate(self.eval_imgs[iou_type], 2)
             create_common_coco_eval(self.coco_eval[iou_type], self.img_ids, self.eval_imgs[iou_type])
 
